221759c8cf83e80db28154b1f7d87e9cc02081a9
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2022-01-22 03:13:58 -0500

    Before implementing openapi spec

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -5,11 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
+// Firebase admin sdk
+var admin = require("firebase-admin");
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: process.env.FIREBASE_RT_DB
+});
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("/");
+ref.once("value", function (snapshot) {
+    console.log(snapshot.val());
+});
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+    return console.log(`Server listening on port ${PORT}...`);
 });
 //# sourceMappingURL=app.js.map
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,25 @@
 import express from 'express';
+import {credential} from "firebase-admin";
+import {getFirestore} from "firebase-admin/lib/firestore";
 const app = express();
-
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
+
+// Firebase admin sdk
+var admin = require("firebase-admin");
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: process.env.FIREBASE_RT_DB
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+
+// Example fetching the entire JSON tree:
+var ref = db.ref("/");
+ref.once("value", function(snapshot) {
+    console.log(snapshot.val());
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
