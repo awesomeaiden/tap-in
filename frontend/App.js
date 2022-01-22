496924f357745f9fbacbe643c09b3e69761b3e41
aboli3514 <39972127+aboli3514@users.noreply.github.com>
2022-01-22 03:11:22 -0500

    front end compiles

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,18 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import * as React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import TestScreen from "./src/screens/TestScreen";
 
-const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-  },
-  {
-    initialRouteName: "Home",
-    defaultNavigationOptions: {
-      title: "App",
-    },
-  }
-);
+const Stack = createNativeStackNavigator();
 
-export default createAppContainer(navigator);
+export default function App() {
+    return (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name = "Home" component={HomeScreen} />
+            <Stack.Screen name = "Test" component={TestScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
+    );
+}
--- frontend/src/screens/TestScreen.js
+++ frontend/src/screens/TestScreen.js
@@ -2,15 +2,15 @@ import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text, Button } from '@ui-kitten/components';
 
-const HomeScreen = ({navigation}) => (
+const TestScreen = ({navigation}) => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>HOME SCREEN</Text>
-    <Button onPress={() => navigation.navigate('Test')}>Button</Button>
+    <Text category='h1'>TEST SCREEN</Text>
+    <Button onPress={() => navigation.navigate('Home')}>Button</Button>
   </Layout>
 );
 
 export default () => (
   <ApplicationProvider {...eva} theme={eva.dark}>
-    <HomeScreen />
+    <TestScreen />
   </ApplicationProvider>
 );
