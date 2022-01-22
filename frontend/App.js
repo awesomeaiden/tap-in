73bc787793ee195b0af312a5342c6e7da280d8ed
aboli3514 <39972127+aboli3514@users.noreply.github.com>
2022-01-22 13:36:11 -0500

    added login and signup

--- frontend/App.js
+++ frontend/App.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/screens/HomeScreen";
-import ScanScreen from "./src/screens/ScanScreen";
-import ProjectScreen from "./src/screens/ProjectScreen";
+import LogInScreen from "./src/screens/LogInScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +12,8 @@ export default function App() {
     <NavigationContainer>
         <Stack.Navigator>
             <Stack.Screen name = "Home" component={HomeScreen} />
-            <Stack.Screen name = "Scan" component={ScanScreen} />
-            <Stack.Screen name = "Project" component={ProjectScreen} />
+            <Stack.Screen name = "Log In" component={LogInScreen} />
+            <Stack.Screen name = "Sign Up" component={SignUpScreen} />
         </Stack.Navigator>
     </NavigationContainer>
     );
